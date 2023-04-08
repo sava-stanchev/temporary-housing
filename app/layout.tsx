0c9a37774f8e5611a95afe4d42f1f90aa4cb8b26
Sava Stanchev <s.stanchev94@gmail.com>
2023-04-08 20:39:55 +0300

    added register modal with a register modal store

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: "Temporary Housing",
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello World" isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
